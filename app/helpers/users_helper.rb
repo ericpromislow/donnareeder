2174e445b13fb0177ca81bb3bf9d18009b38c0e8
Eric Promislow <eric.promislow@suse.com>
2018-07-11 19:33:15 -0700

    render the feeds on the show-user page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,42 @@ module UsersHelper
   def item_num(paginated_thing, index)
     (paginated_thing.current_page.to_i - 1) * (paginated_thing.per_page) + index
   end
+
+  def get_label(node)
+    %w/title description htmlUrl xmlUrl/.each do |name|
+      return sanitize(node[name]) if node[name].present?
+    end
+    "?"
+  end
+
+  def get_attrs(node)
+    attrs = {}
+    %w/feed_type title description xmlUrl htmlUrl/.each do |name|
+      attrs[name] = sanitize(node[name]) if node[name].present?
+    end
+    attrs
+  end
+
+  def dump_the_list(node)
+    results = []
+    dump_the_list_aux(node, results)
+    raw(results.join(''))
+  end
+
+  def dump_the_list_aux(node, results)
+    tree = node.subtree.arrange_serializable(order: :created_at)
+    attrs = get_attrs(node).merge(class: node['node_type']).map{|k, v| %Q[#{k}="#{v}"]}.join(" ")
+    if node['node_type'] == 'feed'
+      results << %Q[<li #{attrs}>#{get_label(node)}</li>\n]
+    else
+      results << %Q[<li #{attrs}>\n]
+      results << %Q[<ul class="ul-nodelist">\n]
+      node.children.each do |subnode|
+        dump_the_list_aux(subnode, results)
+      end
+      results << %Q[</ul>\n]
+      results << %Q[</li>\n]
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,13 @@ end
                password:              password,
                password_confirmation: password)
 end
+
+user = User.find_by_name('a1')
+File.open(File.expand_path("../../test/fixtures/nodes.yml", __FILE__)) do |fd|
+  y = YAML.load(fd.read).deep_symbolize_keys
+  node_languages = Node.create!(y[:languages].merge(user: user))
+  node_python = Node.create!(y[:python].merge(user: user, parent: node_languages))
+  node_javascript = Node.create!(y[:javascript].merge(user: user, parent: node_languages))
+  node_guido = Node.create!(y[:guido].merge(user: user, parent: node_python))
+  node_brendon = Node.create!(y[:brendon].merge(user: user, parent: node_javascript))
+end
